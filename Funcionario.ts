18cf4774a52673a000e587149e3bc271095394d4
Windsor Loss <windsorlossassismonteiro@gmail.com>
2021-09-15 17:39:10 -0300

    Encapsulamento adicionado nas propriedades do funcionario

--- Funcionario.ts
+++ Funcionario.ts
@@ -1,12 +1,12 @@
 import Endereco from "./Endereco"
 
 export default class Funcionario {
-    nome : string
-    matricula : number
-    cpf : string
-    endereco : Endereco
-    email : string
-    telefone : string
+    private nome : string
+    private matricula : number
+    private cpf : string
+    private endereco : Endereco
+    private email : string
+    private telefone : string
 
     constructor(nome : string, cpf : string, endereco : Endereco, email : string, telefone : string) {
         this.nome = nome
